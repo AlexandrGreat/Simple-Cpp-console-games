b4115b0d76354d16ef1cda78f80258adf38d435c
AlexandrGreat <109344446+AlexandrGreat@users.noreply.github.com>
2022-07-19 12:16:43 +0300

    PvP update
    *Updated main menu
    *Added PvP mode
    *GUI update
    *bugfixes
    *Updated "Game Over" screen

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -4,9 +4,9 @@
 #include <iomanip>
 using namespace std;
 int cell[10];
-int selected, player, gameOver, timeLeft, menuSelect = 1, started = 0;
+int selected, player, gameOver, timeLeft, menuSelect = 1, started = 0, change;
 HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
-bool moveMade;
+bool moveMade, pvpMoveMade;
 
 void colorText(int k)
 {
@@ -37,7 +37,13 @@ void launch()
 	colorText(1);
 	cout << "#" << endl;
 
-	for (int j = 0; j < 4; j++)
+	cout << "#" << setw(27);
+	colorText(2);
+	cout << "by AlexandrGreat" << setw(12);
+	colorText(1);
+	cout << "#" << endl;
+
+	for (int j = 0; j < 3; j++)
 	{
 		for (int i = 0; i < 40; i++)
 			if ((i == 0) || (i == 39))cout << "#"; else cout << " ";
@@ -49,10 +55,28 @@ void launch()
 
 	cout << "#              ";
 	if (menuSelect == 1) colorText(5);
-	cout << "Play as X"; colorText(1); cout<< "               #" << endl;
+	cout << "Play as X"; colorText(1); cout << "               #" << endl;
 
 	cout << "#              ";
 	if (menuSelect == 2) colorText(5);
+	cout << "Play as O"; colorText(1); cout << "               #" << endl;
+
+	for (int j = 0; j < 2; j++)
+	{
+		for (int i = 0; i < 40; i++)
+			if ((i == 0) || (i == 39))cout << "#"; else cout << " ";
+		cout << endl;
+	}
+
+	cout << "#" << setw(28);
+	cout << "Play against player" << setw(11) << "#" << endl;
+
+	cout << "#              ";
+	if (menuSelect == 3) colorText(5);
+	cout << "Play as X"; colorText(1); cout << "               #" << endl;
+
+	cout << "#              ";
+	if (menuSelect == 4) colorText(5);
 	cout << "Play as O"; colorText(1); cout << "               #" << endl;
 
 	for (int j = 0; j < 3; j++)
@@ -61,6 +85,7 @@ void launch()
 			if ((i == 0) || (i == 39))cout << "#"; else cout << " ";
 		cout << endl;
 	}
+
 	for (int i = 0; i < 40; i++) cout << "#";
 }
 
@@ -70,19 +95,19 @@ void menuControls()
 		switch (_getch())
 		{
 		case'w':if (menuSelect != 1) menuSelect--; break;
-		case's':if (menuSelect != 2) menuSelect++; break;
+		case's':if (menuSelect != 4) menuSelect++; break;
 		case'e':started = menuSelect; break;
 		}
 }
 
 void cellCheck(int c)
 {
-	if ((cell[c] == 0) && (selected != c)) { colorText(4); cout << " "; colorText(1);};
-	if ((cell[c] == 0) && (selected == c)) { colorText(5); cout << " ";  colorText(4);  colorText(1);};
-	if ((cell[c] == 1) && (selected != c)) { colorText(4); colorText(2); cout << "X"; colorText(1);};
-	if ((cell[c] == 1) && (selected == c)) { colorText(5); cout << "X";  colorText(4); colorText(1);};
-	if ((cell[c] == 2) && (selected != c)) { colorText(4); colorText(3); cout << "O"; colorText(1);};
-	if ((cell[c] == 2) && (selected == c)) {colorText(5); cout << "O";  colorText(4); colorText(1);};
+	if ((cell[c] == 0) && (selected != c)) { colorText(4); cout << " "; colorText(1); };
+	if ((cell[c] == 0) && (selected == c)) { colorText(5); cout << " ";  colorText(4);  colorText(1); };
+	if ((cell[c] == 1) && (selected != c)) { colorText(4); colorText(2); cout << "X"; colorText(1); };
+	if ((cell[c] == 1) && (selected == c)) { colorText(5); cout << "X";  colorText(4); colorText(1); };
+	if ((cell[c] == 2) && (selected != c)) { colorText(4); colorText(3); cout << "O"; colorText(1); };
+	if ((cell[c] == 2) && (selected == c)) { colorText(5); cout << "O";  colorText(4); colorText(1); };
 }
 
 void line(int l)
@@ -101,17 +126,37 @@ void graphics()
 {
 	line(1);
 	line(2);
-	line(3); 
+	line(3);
 	line(2);
 	line(1);
 	line(2);
-	line(4); 
+	line(4);
 	line(2);
 	line(1);
 	line(2);
-	line(5); 
+	line(5);
 	line(2);
 	line(1);
+	colorText(3);
+	cout << "Gamemode: ";
+	colorText(1);
+	switch (started)
+	{
+	case(1):cout << "against computer" << endl; break;
+	case(2):cout << "against computer" << endl; break;
+	case(3):cout << "against player" << endl; break;
+	case(4):cout << "against player" << endl; break;
+	}
+	if (started > 2)
+	{
+		colorText(3);
+		cout << "Player turn: ";
+		colorText(1);
+		if ((change == 1) && (started == 3)) { cout << "X" << endl; }
+		if ((change == 2) && (started == 3)) { cout << "O" << endl; }
+		if ((change == 1) && (started == 4)) { cout << "O" << endl; }
+		if ((change == 2) && (started == 4)) { cout << "X" << endl; }
+	}
 }
 
 void setup()
@@ -137,42 +182,157 @@ void controls(int s)
 		{
 		case 'a': if ((selected == 1) || (selected == 2) || (selected == 4) || (selected == 5) || (selected == 7) || (selected == 8)) selected--; break;
 		case 'd': if ((selected == 0) || (selected == 1) || (selected == 3) || (selected == 4) || (selected == 6) || (selected == 7)) selected++; break;
-		case 'w': if ((selected == 3) || (selected == 4) || (selected == 5) || (selected == 6) || (selected == 7) || (selected == 8)) selected-=3; break;
-		case 's': if ((selected == 0) || (selected == 1) || (selected == 2) || (selected == 3) || (selected == 4) || (selected == 5)) selected+=3; break;
+		case 'w': if ((selected == 3) || (selected == 4) || (selected == 5) || (selected == 6) || (selected == 7) || (selected == 8)) selected -= 3; break;
+		case 's': if ((selected == 0) || (selected == 1) || (selected == 2) || (selected == 3) || (selected == 4) || (selected == 5)) selected += 3; break;
 		case 'e': if (cell[selected] == 0) {
 			cell[selected] = p1;
 			if ((cell[0] > 0) && (cell[1] > 0) && (cell[2] > 0) && (cell[3] > 0) && (cell[4] > 0) && (cell[5] > 0) && (cell[6] > 0) && (cell[7] > 0) && (cell[8] > 0) && (gameOver == 0))
 			{
 				player = 1; gameOver = 3;
-			} else player = 2;
+			}
+			else player = 2;
 		} break;
 		}
 	}
 }
 
+void pvpSetup()
+{
+	timeLeft = 7;
+	gameOver = 0;
+	selected = 0;
+	pvpMoveMade = true;
+	for (int i = 0; i < 9; i++)
+	{
+		cell[i] = 0;
+	}
+}
+
+void pvpControls(int s)
+{
+	int p1, p2;
+	p1 = s - 2;
+	p2 = 5 - s;
+	if (_kbhit())
+	{
+		switch (_getch())
+		{
+		case 'a': if ((selected == 1) || (selected == 2) || (selected == 4) || (selected == 5) || (selected == 7) || (selected == 8)) selected--; break;
+		case 'd': if ((selected == 0) || (selected == 1) || (selected == 3) || (selected == 4) || (selected == 6) || (selected == 7)) selected++; break;
+		case 'w': if ((selected == 3) || (selected == 4) || (selected == 5) || (selected == 6) || (selected == 7) || (selected == 8)) selected -= 3; break;
+		case 's': if ((selected == 0) || (selected == 1) || (selected == 2) || (selected == 3) || (selected == 4) || (selected == 5)) selected += 3; break;
+		case 'e': if (cell[selected] == 0) 
+		{
+			
+			if (pvpMoveMade)
+			{
+				cell[selected] = p1;
+				change = 2;
+			}
+			if ((cell[0] > 0) && (cell[1] > 0) && (cell[2] > 0) && (cell[3] > 0) && (cell[4] > 0) && (cell[5] > 0) && (cell[6] > 0) && (cell[7] > 0) && (cell[8] > 0) && (gameOver == 0))
+			{
+				gameOver = 3;
+			}
+			if (!pvpMoveMade)
+			{
+				cell[selected] = p2;
+				change = 1;
+			}
+			if ((cell[0] > 0) && (cell[1] > 0) && (cell[2] > 0) && (cell[3] > 0) && (cell[4] > 0) && (cell[5] > 0) && (cell[6] > 0) && (cell[7] > 0) && (cell[8] > 0) && (gameOver == 0))
+			{
+				gameOver = 3;
+			}
+		}
+				break;
+		}
+	}
+}
+
+void pvpCheck()
+{
+	if (change==2)
+	{
+		pvpMoveMade = false;
+	}
+	else pvpMoveMade = true;
+}
+
 void rezCheck(int s)
 {
 	int p1, p2;
-	if (s == 1) { p1 = 1; p2 = 2; }
-	if (s == 2) { p1 = 2; p2 = 1; }
-	//Player wins
-	if ((cell[0] == p1) && (cell[1] == p1) && (cell[2] == p1)) gameOver = 1; 
-	if ((cell[3] == p1) && (cell[4] == p1) && (cell[5] == p1)) gameOver = 1; 
-	if ((cell[6] == p1) && (cell[7] == p1) && (cell[8] == p1)) gameOver = 1; 
-	if ((cell[0] == p1) && (cell[3] == p1) && (cell[6] == p1)) gameOver = 1; 
-	if ((cell[1] == p1) && (cell[4] == p1) && (cell[7] == p1)) gameOver = 1; 
-	if ((cell[2] == p1) && (cell[5] == p1) && (cell[8] == p1)) gameOver = 1; 
-	if ((cell[0] == p1) && (cell[4] == p1) && (cell[8] == p1)) gameOver = 1; 
-	if ((cell[2] == p1) && (cell[4] == p1) && (cell[6] == p1)) gameOver = 1; 
-	//Computer wins
-	if ((cell[0] == p2) && (cell[1] == p2) && (cell[2] == p2)) gameOver = 2; 
-	if ((cell[3] == p2) && (cell[4] == p2) && (cell[5] == p2)) gameOver = 2; 
-	if ((cell[6] == p2) && (cell[7] == p2) && (cell[8] == p2)) gameOver = 2; 
-	if ((cell[0] == p2) && (cell[3] == p2) && (cell[6] == p2)) gameOver = 2; 
-	if ((cell[1] == p2) && (cell[4] == p2) && (cell[7] == p2)) gameOver = 2; 
-	if ((cell[2] == p2) && (cell[5] == p2) && (cell[8] == p2)) gameOver = 2; 
-	if ((cell[0] == p2) && (cell[4] == p2) && (cell[8] == p2)) gameOver = 2; 
-	if ((cell[2] == p2) && (cell[4] == p2) && (cell[6] == p2)) gameOver = 2; 
+	if (s<3) 
+	{ 
+		p1 = s;
+		p2 = 3-s;
+	}
+	if (s > 2)
+	{
+		p1 = s - 2;
+		p2 = 5 - s;
+	}
+	if (s < 3)
+	{
+		//Player wins
+		if ((cell[0] == p1) && (cell[1] == p1) && (cell[2] == p1)) gameOver = 1;
+		if ((cell[3] == p1) && (cell[4] == p1) && (cell[5] == p1)) gameOver = 1;
+		if ((cell[6] == p1) && (cell[7] == p1) && (cell[8] == p1)) gameOver = 1;
+		if ((cell[0] == p1) && (cell[3] == p1) && (cell[6] == p1)) gameOver = 1;
+		if ((cell[1] == p1) && (cell[4] == p1) && (cell[7] == p1)) gameOver = 1;
+		if ((cell[2] == p1) && (cell[5] == p1) && (cell[8] == p1)) gameOver = 1;
+		if ((cell[0] == p1) && (cell[4] == p1) && (cell[8] == p1)) gameOver = 1;
+		if ((cell[2] == p1) && (cell[4] == p1) && (cell[6] == p1)) gameOver = 1;
+		//Computer wins
+		if ((cell[0] == p2) && (cell[1] == p2) && (cell[2] == p2)) gameOver = 2;
+		if ((cell[3] == p2) && (cell[4] == p2) && (cell[5] == p2)) gameOver = 2;
+		if ((cell[6] == p2) && (cell[7] == p2) && (cell[8] == p2)) gameOver = 2;
+		if ((cell[0] == p2) && (cell[3] == p2) && (cell[6] == p2)) gameOver = 2;
+		if ((cell[1] == p2) && (cell[4] == p2) && (cell[7] == p2)) gameOver = 2;
+		if ((cell[2] == p2) && (cell[5] == p2) && (cell[8] == p2)) gameOver = 2;
+		if ((cell[0] == p2) && (cell[4] == p2) && (cell[8] == p2)) gameOver = 2;
+		if ((cell[2] == p2) && (cell[4] == p2) && (cell[6] == p2)) gameOver = 2;
+	}
+	if (s == 3)
+	{
+		//Player X wins
+		if ((cell[0] == p1) && (cell[1] == p1) && (cell[2] == p1)) gameOver = 4;
+		if ((cell[3] == p1) && (cell[4] == p1) && (cell[5] == p1)) gameOver = 4;
+		if ((cell[6] == p1) && (cell[7] == p1) && (cell[8] == p1)) gameOver = 4;
+		if ((cell[0] == p1) && (cell[3] == p1) && (cell[6] == p1)) gameOver = 4;
+		if ((cell[1] == p1) && (cell[4] == p1) && (cell[7] == p1)) gameOver = 4;
+		if ((cell[2] == p1) && (cell[5] == p1) && (cell[8] == p1)) gameOver = 4;
+		if ((cell[0] == p1) && (cell[4] == p1) && (cell[8] == p1)) gameOver = 4;
+		if ((cell[2] == p1) && (cell[4] == p1) && (cell[6] == p1)) gameOver = 4;
+		//Computer O wins
+		if ((cell[0] == p2) && (cell[1] == p2) && (cell[2] == p2)) gameOver = 5;
+		if ((cell[3] == p2) && (cell[4] == p2) && (cell[5] == p2)) gameOver = 5;
+		if ((cell[6] == p2) && (cell[7] == p2) && (cell[8] == p2)) gameOver = 5;
+		if ((cell[0] == p2) && (cell[3] == p2) && (cell[6] == p2)) gameOver = 5;
+		if ((cell[1] == p2) && (cell[4] == p2) && (cell[7] == p2)) gameOver = 5;
+		if ((cell[2] == p2) && (cell[5] == p2) && (cell[8] == p2)) gameOver = 5;
+		if ((cell[0] == p2) && (cell[4] == p2) && (cell[8] == p2)) gameOver = 5;
+		if ((cell[2] == p2) && (cell[4] == p2) && (cell[6] == p2)) gameOver = 5;
+	}
+	if (s == 4)
+	{
+		//Player O wins
+		if ((cell[0] == p1) && (cell[1] == p1) && (cell[2] == p1)) gameOver = 5;
+		if ((cell[3] == p1) && (cell[4] == p1) && (cell[5] == p1)) gameOver = 5;
+		if ((cell[6] == p1) && (cell[7] == p1) && (cell[8] == p1)) gameOver = 5;
+		if ((cell[0] == p1) && (cell[3] == p1) && (cell[6] == p1)) gameOver = 5;
+		if ((cell[1] == p1) && (cell[4] == p1) && (cell[7] == p1)) gameOver = 5;
+		if ((cell[2] == p1) && (cell[5] == p1) && (cell[8] == p1)) gameOver = 5;
+		if ((cell[0] == p1) && (cell[4] == p1) && (cell[8] == p1)) gameOver = 5;
+		if ((cell[2] == p1) && (cell[4] == p1) && (cell[6] == p1)) gameOver = 5;
+		//Computer X wins
+		if ((cell[0] == p2) && (cell[1] == p2) && (cell[2] == p2)) gameOver = 4;
+		if ((cell[3] == p2) && (cell[4] == p2) && (cell[5] == p2)) gameOver = 4;
+		if ((cell[6] == p2) && (cell[7] == p2) && (cell[8] == p2)) gameOver = 4;
+		if ((cell[0] == p2) && (cell[3] == p2) && (cell[6] == p2)) gameOver = 4;
+		if ((cell[1] == p2) && (cell[4] == p2) && (cell[7] == p2)) gameOver = 4;
+		if ((cell[2] == p2) && (cell[5] == p2) && (cell[8] == p2)) gameOver = 4;
+		if ((cell[0] == p2) && (cell[4] == p2) && (cell[8] == p2)) gameOver = 4;
+		if ((cell[2] == p2) && (cell[4] == p2) && (cell[6] == p2)) gameOver = 4;
+	}
 }
 
 void priority(int s)
@@ -253,8 +413,8 @@ void priority(int s)
 void notPriority(int s)
 {
 	int p1, p2;
-	if (s == 1) p2 = 2; 
-	if (s == 2) p2 = 1; 
+	if (s == 1) p2 = 2;
+	if (s == 2) p2 = 1;
 	int randomCell = rand() % 9;
 	if (cell[randomCell] != 0) notPriority(started); else cell[randomCell] = p2;
 }
@@ -262,37 +422,51 @@ void notPriority(int s)
 void logic()
 {
 	int randomCell;
-	if ((player == 2) && (gameOver==0))
+	if ((player == 2) && (gameOver == 0))
 	{
 		priority(started);
 		if (!moveMade)
-		notPriority(started);
+			notPriority(started);
 	}
 	moveMade = false;
 	if ((cell[0] > 0) && (cell[1] > 0) && (cell[2] > 0) && (cell[3] > 0) && (cell[4] > 0) && (cell[5] > 0) && (cell[6] > 0) && (cell[7] > 0) && (cell[8] > 0) && (gameOver == 0))
 	{
 		player = 1; gameOver = 3;
-	} else player = 1;
+	}
+	else player = 1;
 }
 
 int main()
 {
-	while (started==0)
+	while (started == 0)
 	{
 		launch();
 		menuControls();
 		Sleep(10);
 		system("cls");
 	}
-	setup();
-	while (gameOver==0)
+	if (started < 3) setup();
+	if (started > 2) pvpSetup();
+	while (gameOver == 0)
 	{
-		logic();
-		rezCheck(started);
-		graphics();
-		controls(started);
-		Sleep(10);
-		system("cls");
+		if (started < 3)
+		{
+			logic();
+			rezCheck(started);
+			graphics();
+			controls(started);
+			Sleep(10);
+			system("cls");
+		}
+		if (started > 2)
+		{
+			pvpControls(started);
+			pvpCheck();
+			rezCheck(started);
+			graphics();
+			Sleep(10);
+			system("cls");
+		}
 	}
 	system("cls");
 
@@ -313,16 +487,19 @@ int main()
 		colorText(1);
 		cout << "#" << endl;
 
-		int w1,w2;
+		int w1, w2;
 		if (gameOver == 1) w1 = 22;
 		if (gameOver == 2) w1 = 24;
 		if (gameOver == 3) w1 = 20;
+		if ((gameOver == 4) || (gameOver == 5)) w1 = 25;
 		cout << "#" << setw(w1);
-		if (gameOver == 1) { colorText(3); cout << "You Win!"; w2 = 17; colorText(1);}
-		if (gameOver == 2) { colorText(3); cout << "Computer Won!"; w2 = 15; colorText(1);}
-		if (gameOver == 3) { colorText(3); cout << "Draw!"; w2 = 19; colorText(1);}
+		if (gameOver == 1) { colorText(3); cout << "You Win!"; w2 = 17; colorText(1); }
+		if (gameOver == 2) { colorText(3); cout << "Computer Won!"; w2 = 15; colorText(1); }
+		if (gameOver == 3) { colorText(3); cout << "Draw!"; w2 = 19; colorText(1); }
+		if (gameOver == 4) { colorText(3); cout << "Player X wins!"; w2 = 14; colorText(1); }
+		if (gameOver == 5) { colorText(3); cout << "Player O wins!"; w2 = 14; colorText(1); }
 		colorText(1);
-		cout <<setw(w2)<< "#" << endl;
+		cout << setw(w2) << "#" << endl;
 
 		for (int j = 0; j < 3; j++)
 		{
